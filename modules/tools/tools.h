1e11c17225f138b7b04b30f43f1dceec54fddf10
dolivera <daniel_olivera@sfu.ca>
2023-01-30 19:30:21 -0800

    Add modules directory and relevant modules

---
+++ modules/tools/tools.h
@@ -0,0 +1,8 @@
+//Copied over from assignment 1
+#include <stdlib.h>
+#include <time.h>
+
+long long getTimeInMs(void);
+void sleepForMs (long long delayInMs);
+long long getRandomTimeInMs(void);
+int getRandomZeroOrOne(void);
